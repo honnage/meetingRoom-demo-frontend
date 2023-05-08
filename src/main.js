e5fb6b78edd2cf93fd651a12de9c0c10f2c843db
honnage <hon_da-1998@hotmail.com>
2023-05-08 11:01:34 +0700

    update package

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue'
+
+createApp(App).use(store).use(router).use(Antd).mount('#app')
