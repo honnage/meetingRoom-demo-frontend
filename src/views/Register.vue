740e4e7335708e01056936725677123eaad6229a
honnage <hon_da-1998@hotmail.com>
2023-05-25 00:51:51 +0700

    register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -12,9 +12,7 @@
               name="basic"
               :label-col="{ span: 8 }"
               :wrapper-col="{ span: 24 }"
-              autocomplete="off"
-              @finish="onFinish"
-              @finishFailed="onFinishFailed"
+              @submit.prevent="onSubmit()"
             >
               <a-row :gutter="24">
                 <a-col :xl="24" :lg="24" :md="12" :sm="24">
@@ -43,11 +41,15 @@
                           required: true,
                           message: 'Please input your password!',
                         },
+                        {
+                          pattern: /^(?=.*[a-z])[a-zA-Z0-9]{8,15}$/,
+                          message:
+                            'Password be between 8 and 15 characters long',
+                          // รหัสผ่านมีความยาวระหว่าง 8 ถึง 15 ตัวอักษร
+                        },
                       ]"
                     >
-                      <label for="u_password" class="label">
-                        รหัสผ่าน
-                      </label>
+                      <label for="u_password" class="label"> รหัสผ่าน </label>
                       <a-input-password v-model:value="formState.u_password" />
                     </a-form-item>
                   </div>
@@ -62,9 +64,7 @@
                         },
                       ]"
                     >
-                      <label for="u_firstname" class="label">
-                        ชื่อ
-                      </label>
+                      <label for="u_firstname" class="label"> ชื่อ </label>
                       <a-input v-model:value="formState.u_firstname" />
                     </a-form-item>
                   </div>
@@ -79,9 +79,7 @@
                         },
                       ]"
                     >
-                      <label for="u_lastname" class="label">
-                        นามสกุล
-                      </label>
+                      <label for="u_lastname" class="label"> นามสกุล </label>
                       <a-input v-model:value="formState.u_lastname" />
                     </a-form-item>
                   </div>
@@ -98,7 +96,6 @@
                 </button>
               </div>
             </a-form>
-
           </div>
         </div>
       </div>
@@ -108,17 +105,20 @@
 </template>
 <script>
 import { defineComponent } from "vue";
+import axios from "axios";
+
 export default defineComponent({
   name: "RegisterPage",
   setup() {
-    const onFinish = values => {
-      console.log('Success:', values);
+    const onFinish = async (values) => {
+      console.log("Success:", values);
     };
-    const onFinishFailed = errorInfo => {
-      console.log('Failed:', errorInfo);
+
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
     };
+
     return {
-      // formState,
       onFinish,
       onFinishFailed,
     };
@@ -131,7 +131,110 @@ export default defineComponent({
         u_firstname: "",
         u_lastname: "",
       },
+      errorMessage: null,
+      checkUsername: false,
+      checkPassword: false,
+      checkFirstname: false,
+      checkLastname: false,
     };
+  },
+  methods: {
+    onSubmit() {
+      // console.log("onSubmit", this.formState);
+
+      // /^(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9]{8,15}$/,
+      // 'Password must contain at least one digit and be between 8 and 15 characters long',
+      //  รหัสผ่านต้องประกอบด้วยตัวเลขอย่างน้อย 1 ตัวและมีความยาวระหว่าง 8 ถึง 15 ตัวอักษร
+
+      // /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,15}$/,
+      // 'Password must contain at least one uppercase letter, at least one digit, and be between 8 and 15 characters long',
+      // รหัสผ่านต้องประกอบด้วยตัวอักษรพิมพ์ใหญ่อย่างน้อย 1 ตัว, ตัวเลขอย่างน้อย 1 ตัว และมีความยาวระหว่าง 8 ถึง 15 ตัวอักษร
+
+      const username = this.formState.u_username;
+      const password = this.formState.u_password;
+      const firstname = this.formState.u_firstname;
+      const lastname = this.formState.u_lastname;
+      const regexPassword = /^(?=.*[a-z])[a-zA-Z0-9]{8,15}$/;
+
+      const isValidPassword = regexPassword.test(password);
+
+      if (isValidPassword) {
+        // console.log("รหัสผ่านถูกต้อง")
+        this.checkPassword = true;
+      } else {
+        // console.log("รหัสผ่านไม่ถูกต้อง")
+        this.checkPassword = false;
+      }
+
+      if (username != "") {
+        this.checkUsername = true;
+      } else {
+        this.checkUsername = false;
+      }
+
+      if (firstname != "") {
+        this.checkFirstname = true;
+      } else {
+        this.checkFirstname = false;
+      }
+
+      if (lastname != "") {
+        this.checkLastname = true;
+      } else {
+        this.checkLastname = false;
+      }
+
+      // console.log("checkUsername", this.checkUsername);
+      // console.log("checkPassword", this.checkPassword);
+      // console.log("checkFirstname", this.checkFirstname);
+      // console.log("checkLastname", this.checkLastname);
+
+      if (
+        this.checkUsername == true &&
+        this.checkPassword == true &&
+        this.checkFirstname == true &&
+        this.checkLastname == true
+      ) {
+        console.log("this.formState", this.formState);
+        axios
+          // .post(`http://localhost:3066/api/account/register`, this.formState)
+          .post(`api/account/register`, this.formState)
+
+          .then((response) => {
+            console.log("response", response);
+            this.openNotificationWithIcon(
+              "success",
+              "Register success",
+              "Save data successfully"
+            );
+            this.onReset();
+          })
+          .catch((err) => {
+            this.openNotificationWithIcon(
+              "error",
+              err.request.statusText,
+              err.response.data.message
+            );
+            console.log("error", err);
+          });
+      }
+    },
+
+    onReset() {
+      this.formState = {
+        u_username: "",
+        u_password: "",
+        u_firstname: "",
+        u_lastname: "",
+      };
+    },
+
+    openNotificationWithIcon(type, title, details) {
+      this.$notification[type]({
+        message: `${title}`,
+        description: `${details}`,
+      });
+    },
   },
 });
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3066/',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+}
